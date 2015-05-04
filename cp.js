146abeb902b4ee8201d9ab3ec96b77a6e7166bd7
Vasil Toshkov <cloxy@cloxy.com>
2015-05-04 19:46:25 +0300

    Added code copy protection
    The script now protects source code in text from poisoning.

--- cp.js
+++ cp.js
@@ -1,4 +1,4 @@
-/* CopyPoison 3.1 | (c) 2015 MIT license | http://copypoison.com */
+/* CopyPoison 3.2 | (c) 2015 MIT license | http://copypoison.com */
 (function() {
 	var chars = 'ABEKMHOΠPCTXaeĸopcyxIiJjSs'+'АВЕКМНОПРСТХаекорсухІіЈјЅѕ';
 	var cfg = {length : 150, link : 'true', prefix : '', suffix : ''}, script;
@@ -23,11 +23,18 @@
 		return(ptext);
 	}
 
+	function isCode(text) {
+		return(
+			(text.indexOf('<') != -1 && text.indexOf('>') != -1) ||
+			(text.indexOf('{') != -1 && text.indexOf('}') != -1)
+		);
+	}
+
 	function copyPoison() {
 		if(window.clipboardData && document.selection) { // IE
 			var selectionObject = document.selection.createRange();
 			var selectionText = selectionObject.text;
-			if(selectionText.length > cfg.length) {
+			if(selectionText.length > cfg.length && !isCode(selectionText)) {
 				selectionText = replaceChars(selectionText);
 				if(cfg.link == 'true') {
 					selectionText+= "\r\n\r\n"+cfg.prefix+document.location.href+cfg.suffix;
@@ -38,7 +45,7 @@
 		} else {
 			var selectionObject = window.getSelection();
 			var selectionText = selectionObject.toString();
-			if(selectionText.length > cfg.length) {
+			if(selectionText.length > cfg.length && !isCode(selectionText)) {
 				selectionText = replaceChars(selectionText);
 				if(cfg.link == 'true') {
 					selectionText+= "\n\n"+cfg.prefix+document.location.href+cfg.suffix;
